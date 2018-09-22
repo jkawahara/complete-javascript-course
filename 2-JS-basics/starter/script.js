a37940665fc3126887cffb4812d52f28fc512a2c
John Kawahara <kawaharajohn@gmail.com>
2018-09-22 16:49:55 -0700

    Complete Sec2 code challenge 3 and add script.js ref to index.html

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -72,3 +72,42 @@ switch (true) {
     console.log('John, with an average score of ' + aveScoreJohn + ', Mike, with an average score of ' + aveScoreMike + ' and Mary, with an average score of ' + aveScoreMary + ', all win with tied highest scores!');
   }
 */
+
+ /*****************************
+* CODING CHALLENGE 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+/*
+function tipCalculator(bill) {
+  if (bill < 50) {
+    percentage = .20;
+  } else if (bill >= 50 && bill <= 200) {
+      percentage = .15;
+  } else {
+      percentage = .10;
+  }
+  return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]),
+            tipCalculator(bills[1]),
+            tipCalculator(bills[2])];
+var finalAmounts = [bills[0] + tips[0],
+                    bills[1] + tips[1],
+                    bills[2] + tips[2]];
+console.log(tips, finalAmounts);
+*/
